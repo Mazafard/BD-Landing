dd0413ee92a54a591f8a5112979373592e55b406
Mazafard <mazafard@gmail.com>
2023-02-05 16:12:58 +0000

    inti

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -5,7 +5,6 @@ import Divider from './Divider';
 
 const Product = () => {
   const { product } = config;
-  const [firstItem, secondItem] = product.items;
 
   return (
     <section className={`bg-background py-8`} id="product">
@@ -23,42 +22,45 @@ const Product = () => {
           ))}
         </h1>
         <Divider />
-        <div className={`flex flex-wrap`}>
-          <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
-            <h3
-              className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+        {product.items.map((item, index) => {
+          if (index % 2 === 0) {
+            return (
+              <div className={`flex flex-wrap`} key={index}>
+                <div className={`w-5/6 sm:w-1/2 p-6 mt-20`}>
+                  <h3
+                    className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                  >
+                    {item?.title}
+                  </h3>
+                  <p className={`text-gray-600`}>{item?.description}</p>
+                </div>
+                <div className={`w-full sm:w-1/2 p-6`}>
+                  <img className="h-6/6" src={item?.img} alt={item?.title} />
+                </div>
+              </div>
+            );
+          }
+          return (
+            <div
+              className={`flex flex-wrap flex-col-reverse sm:flex-row`}
+              key={index}
             >
-              {firstItem?.title}
-            </h3>
-            <p className={`text-gray-600`}>{firstItem?.description}</p>
-          </div>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={firstItem?.img}
-              alt={firstItem?.title}
-            />
-          </div>
-        </div>
-        <div className={`flex flex-wrap flex-col-reverse sm:flex-row`}>
-          <div className={`w-full sm:w-1/2 p-6`}>
-            <img
-              className="h-6/6"
-              src={secondItem?.img}
-              alt={secondItem?.title}
-            />
-          </div>
-          <div className={`w-full sm:w-1/2 p-6 mt-20`}>
-            <div className={`align-middle`}>
-              <h3
-                className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
-              >
-                {secondItem?.title}
-              </h3>
-              <p className={`text-gray-600 mb-8`}>{secondItem?.description}</p>
+              <div className={`w-full sm:w-1/2 p-6`}>
+                <img className="h-6/6" src={item?.img} alt={item?.title} />
+              </div>
+              <div className={`w-full sm:w-1/2 p-6 mt-20`}>
+                <div className={`align-middle`}>
+                  <h3
+                    className={`text-3xl text-gray-800 font-bold leading-none mb-3`}
+                  >
+                    {item?.title}
+                  </h3>
+                  <p className={`text-gray-600 mb-8`}>{item?.description}</p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </section>
   );
